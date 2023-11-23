85dfd2950a32cf6b1a47ca15a189705c1dc3bda6
Gydra <102178372+otmorozika17101978@users.noreply.github.com>
2023-11-23 15:45:57 +0300

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,66 @@
+    return data
+
+    data = generate_random_data()
+
+    for item in data:
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+
+if __name__ == "__main__":
+    main()
+def main():
+        print(f"Random Number: {item}")
+
+def generate_random_data():    return data
+if __name__ == "__main__":
+
+
+    data = generate_random_data()
+import random
+        print(f"Random Number: {item}")
+
+    main()
+    for item in data:
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+def generate_random_data():
+        print(f"Random Number: {item}")
+def main():
+    for item in data:
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+
+import random
+
+
+    return data
+    data = generate_random_data()
+def generate_random_data():
+if __name__ == "__main__":    main()
+    data = generate_random_data()
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+
+def main():
+
+if __name__ == "__main__":
+
+        print(f"Random Number: {item}")
+def generate_random_data():
+    return data
+
+import random
+def main():
+
+if __name__ == "__main__":
+    for item in data:
+    data = generate_random_data()
+
+        print(f"Random Number: {item}")
+    main()
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
